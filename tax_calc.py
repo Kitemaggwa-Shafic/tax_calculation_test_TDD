acf6db893370e953ff1b086015307447aba1e01e
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-06-29 12:00:10 +0300

    TDD TEST TAX CALCULATION test_two_tax PASSED after defining cal_tax logic file

--- tax_calc.py
+++ tax_calc.py
@@ -1,8 +1,10 @@
 # Tax calculation logic
 
-def cal_tax(salary):
+def cal_tax(earnings):
     # tax =0
-    if(salary <= 12000):
+    if(earnings <= 12000):
         tax = 0
         return tax
- 
+    elif (earnings > 12000 and earnings <= 36000):
+        tax = (0.2)
+        return tax
--- test_tax.py
+++ test_tax.py
@@ -2,3 +2,6 @@ from tax_calc import cal_tax
 
 def test_one_tax():
     assert cal_tax(5000) == 0
+
+def test_two_tax():
+    assert cal_tax(23500) == (20/100)
